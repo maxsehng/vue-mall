2d7563ba7d43165601045f9113da548afa07ee63
unknown <734614385@qq.com>
2019-11-19 18:06:01 +0800

    change

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,39 @@
 <template>
-    <h2>主页</h2>
+    <div id="home">
+        <nav-bar class="home-nav">
+            <div slot="center">购物街</div>
+        </nav-bar>
+    </div>
 </template>
 
 <script>
+
+import NavBar from 'components/common/NavBar/NavBar.vue'
+import {getHomeMultidate} from 'network/home'
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+        NavBar
+    },
+    data(){
+        return {
+            banners:[],
+            recommends:[]
+        }
+    },
+    created(){
+        getHomeMultidate().then( res => {
+            this.banner = res.data.banners.list,
+            this.recommends = res.data.recommends.list
+        })
+    }
 }
 </script>
 
 <style scoped>
-
+    .home-nav{
+        background-color: var(--color-tint);
+        color: aliceblue;
+    }
 </style>
+
